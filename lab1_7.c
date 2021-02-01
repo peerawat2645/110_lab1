6d815bcb2afd4749edf5df9bcbe15274d8b59134
Peerawat Wongmek <peerawat.won@ku.th>
2021-02-01 19:11:58 +0700

    Update lab1_7.c

--- lab1_7.c
+++ lab1_7.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int hour,min,morning;
+    int hour,min,x;
     float time;
     scanf("%f",&time);
     hour=(int)time;
     min=(time-hour)*100;
-     if (hour > 23 || min > 59)
-    {
-        return 1;
-    }
     if (hour >= 12)
     {
-        morning = 1;
+        x = 1;
         if (hour > 12)
-        {
             hour -= 12;
-        }
     }
     else
-    {
-        morning = 0;
-    }
-    if (morning == 0)
-    {
+        x = 0;
+    if (x == 0)
         printf("%d:%02d a.m.", hour, min);
-    }
     else
-    {
         printf("%d:%02d p.m.", hour, min);
-    }
     return 0;
 }
