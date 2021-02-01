83c92ca6c2484a8a80ef2ab3ddcc94514a1dde91
Peerawat Wongmek <peerawat.won@ku.th>
2021-02-01 19:23:56 +0700

    Update lab1_7.c

--- lab1_7.c
+++ lab1_7.c
@@ -6,14 +6,14 @@ int main()
     scanf("%f",&time);
     hour=(int)time;
     min=(time-hour)*100;
-    if (hour >= 12)
+    if(hour==24)
     {
-        x = 1;
-        if(hour==24)
-        {
             x=0;
             hour=0;
-        }
+    }
+    else if (hour >= 12)
+    {
+        x = 1;
         if (hour > 12)
             hour -= 12;
     }
